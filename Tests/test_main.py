82b6d8ab04e1c3e6c6651c6d668100486a616f03
joakimjoh <joakimjoh@hotmail.com>
2025-01-16 12:41:06 +0100

    robot arm

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import pyrealsense2 as rs
 from test_cam import get_left_right, get_line_straight
+from move_robot import move_to_xyz_with_gripper
             
 # Initialize RealSense pipeline
 pipeline = rs.pipeline()
@@ -13,7 +14,11 @@ config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)  # RGB strea
 
 x1, y1, z1, x2, y2, z2 = get_left_right(pipeline, config)
 
+xyz = []
+xyz.append(x1, y1, z1)
 print(x1, y1, z1, x2, y2, z2)
+
+move_to_xyz_with_gripper(xyz)
 
 straight = get_line_straight(pipeline, config)
 
